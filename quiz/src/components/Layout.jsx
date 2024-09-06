c97fee4a70e7d8e4b31e23488ea04c6ccade1052
Nedžma Imamović <nedzma.imamovic.23@size.ba>
2024-09-06 21:35:42 +0200

    layout

--- quiz/src/components/Layout.jsx
+++ quiz/src/components/Layout.jsx
@@ -4,10 +4,12 @@ import { Footer } from "./Footer";
 export const Layout = ({ children }) => {
   return (
     <>
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-cover" style={{backgroundImage: `url(/src/assets/background.avif)`, opacity: `20`}} >
+    <div className="flex flex-col min-h-screen absolute inset-0 bg-black bg-opacity-50 filter backdrop-blur-sm">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
+      </div>
       </div>
     </>
   );
